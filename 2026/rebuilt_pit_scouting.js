db44627592a9685b43cc3e97dde801c58c9c480f
FRC3098-dot <frc3098guest@gmail.com>
2026-03-07 13:26:04 -0500

    Update drivetrain options and remove unused fields
    Removed fields for Width and Weight, added Drivetrain options.

--- 2026/rebuilt_pit_scouting.js
+++ 2026/rebuilt_pit_scouting.js
@@ -9,22 +9,16 @@ var config_data = `
       "code": "t",
       "type": "number"
     },
-    { "name": "Width",
-      "code": "wid",
-      "type": "number",
-      "defaultValue": "0"
-    },
-    { "name": "Weight",
-      "code": "wei",
-      "type": "number",
-      "defaultValue": "0"
+       { "name": "Drivetrain",
+      "code": "drv",
+      "type": "radio",
+      "choices": {
+        "s": "Swerve<br>",
+        "w": "West Coast/Tank<br>",
+        "b": "Butterfly/Grashopper<br>",
+        "m": "Mechanum<br>",
+        "o": "Other"
       },
-    { "name": "Other Drivetrain",
-      "code": "odt",
-      "type": "text",
-      "size": 20,
-      "maxSize": 50
-    },
     { "name": "Floor pickup Fuel",
       "code": "fpu",
       "type": "bool"
