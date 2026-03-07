d0f697b5f363f156447ff95e6d822df6abdd79f4
FRC3098-dot <frc3098guest@gmail.com>
2026-03-07 12:51:47 -0500

    Fix syntax for 'What did the Robot do During Inactive Time'

--- 2026/rebuilt_config.js
+++ 2026/rebuilt_config.js
@@ -153,8 +153,8 @@ var config_data = `
       "code": "tff",
       "type": "bool"
     },
-     "What did the Robot do During Inactive Time":,
-     { "name": "Pass Fuel via Shooting",
+     {"name": "What did the Robot do During Inactive Time":,
+     "name": "Pass Fuel via Shooting",
       "code": "tintPS",
       "type": "bool"
     },  
