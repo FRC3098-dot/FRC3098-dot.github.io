5cc1e3058ec4df36c35e0baa9940c2b76704fbdd
FRC3098-dot <frc3098guest@gmail.com>
2026-03-07 12:58:12 -0500

    Remove drivetrain fields from pit scouting form
    Removed several fields related to drivetrain specifications from the pit scouting form.

--- 2026/rebuilt_pit_scouting.js
+++ 2026/rebuilt_pit_scouting.js
@@ -25,34 +25,6 @@ var config_data = `
       "size": 20,
       "maxSize": 50
     },
-    { "name": "Swerve Ratio",
-      "code": "sr",
-      "type": "radio",
-      "choices": {
-        "1": "L1 (8.14:1)<br>",
-        "2": "L2 (6.75:1)<br>",
-        "3": "L3 (6.12:1)<br>",
-        "4": "L4 (5.14:1)<br>",
-        "o": "Other ratio (put in comments)<br>",
-        "x": "Not Swerve"
-      },
-      "defaultValue":"x"
-    },
-    { "name": "Drivetrain Motor",
-      "code": "mot",
-      "type": "radio",
-      "choices": {
-        "n": "Neo<br>",
-        "f": "Falcon<br>",
-        "c": "CIM<br>",
-        "x": "Other<br>"
-      },
-      "defaultValue":"x"
-    },
-    { "name": "# of Batteries",
-      "code": "nob",
-      "type": "number"
-    },
     { "name": "Floor pickup Fuel",
       "code": "fpu",
       "type": "bool"
